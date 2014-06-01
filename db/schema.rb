277ec7c9c72b885c742d71b36d3a5b623d445093
Anthony Foster <anthonyfoster0506@gmail.com>
2014-06-01 11:28:24 -0700

    cleaned up pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140524002158) do
+ActiveRecord::Schema.define(version: 20140524234356) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
